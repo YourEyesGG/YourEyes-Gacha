f76aaa1f883933664d788e3c941c2f15c698a7af
YourEyes <72436392+YourEyesGG@users.noreply.github.com>
2020-12-25 12:07:50 +0700

    Update server.lua

--- server.lua
+++ server.lua
@@ -8,26 +8,25 @@ ESX.RegisterUsableItem('vouchergacha', function(source)
   local chance = math.random(1, 15) -- Chance GACHA
 	
 	if xPlayer.getInventoryItem('vouchergacha').count >= 1 then
-	  TriggerClientEvent('youreyes-gacha:unboxing', source) -- Trigger Animasi yang di CLIENT
+		TriggerClientEvent('youreyes-gacha:unboxing', source) -- Trigger Animasi yang di CLIENT
     
-	  Citizen.Wait(4500)
-	
-    if chance == 1 then -- Jika Chancenya 1
-       TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'Kamu Memenangkan : '}) -- Notifikasi Chance 1
-		   -- Hasil Taro disini
-    elseif chance == 2 then -- Jika Chancenya 2
-		   TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'Kamu Memenangkan : '}) -- Notifikasi Chance 2
-       -- Hasil Taro disini
-	  elseif chance == 3 then -- Jika Chancenya 3
-		  TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'Kamu Memenangkan : '}) -- Notifikasi Chance 3
-		  -- Hasil Taro disini
-	  elseif chance == 5 then -- Jika Chancenya 5
-		  TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'Kamu Memenangkan : '}) -- Notifikasi Chance 5
-		  -- Hasil Taro disini
-	else -- Jika Chance Selain 1,2,3,5
-		TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'ZONK!!'}) -- Notifikasi GAGAL
-	end
+	  	Citizen.Wait(4500)
+		if chance == 1 then -- Jika Chancenya 1
+       			TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'Kamu Memenangkan : '}) -- Notifikasi Chance 1
+		   	-- Hasil Taro disini
+    		elseif chance == 2 then -- Jika Chancenya 2
+		   	TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'Kamu Memenangkan : '}) -- Notifikasi Chance 2
+       			-- Hasil Taro disini
+	  	elseif chance == 3 then -- Jika Chancenya 3
+		  	TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'Kamu Memenangkan : '}) -- Notifikasi Chance 3
+		  	-- Hasil Taro disini
+	 	elseif chance == 5 then -- Jika Chancenya 5
+		  	TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'Kamu Memenangkan : '}) -- Notifikasi Chance 5
+		  	-- Hasil Taro disini
+		else -- Jika Chance Selain 1,2,3,5
+			TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'ZONK!!'}) -- Notifikasi GAGAL
+		end
 
-	xPlayer.removeInventoryItem('vouchergacha', 1) -- Remove Item Voucher dari Inventory
+		xPlayer.removeInventoryItem('vouchergacha', 1) -- Remove Item Voucher dari Inventory
 	end
 end)
